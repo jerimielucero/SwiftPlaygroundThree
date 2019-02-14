deacf4e13638043b12d3d4507a498f8db085a766
Jerimie Lucero <jerimie@hawaii.edu>
2019-02-14 10:28:07 -1000

    Completed SwiftPlaygroundThree

--- SwiftPlaygroundThree.playground/Contents.swift
+++ SwiftPlaygroundThree.playground/Contents.swift
@@ -1,3 +1,17 @@
 import UIKit
 
 var str = "Hello, playground"
+
+//part 2
+//declaration
+var fruitNames : [String] = ["Grapes","Mango","Strawberries"]
+//part 4
+print(fruitNames)
+
+print("I like to eat\(fruitNames)")
+//seperating names
+for name in fruitNames {print("I like to eat" + name)}
+
+print(fruitNames[1])
+
+print(fruitNames[0])
